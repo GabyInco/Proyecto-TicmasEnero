429b112d156a938ef2189608685471096a9f293c
GabyInco <123882094+GabyInco@users.noreply.github.com>
2023-01-30 16:09:26 -0300

    Add files via upload

---
+++ Proyecto_Final/intento1/function.js
@@ -0,0 +1,17 @@
+
+document.getElementById("nombre").onclick = function () {
+    document.getElementById("nombre").innerHTML= "Gabriela Sabrina";
+}
+
+document.getElementById("ciudad").onclick = function () {
+    document.getElementById("ciudad").innerHTML= "Florencio Varela, Buenos Aires";
+}
+
+document.getElementById("telefono").onclick = function () {
+    document.getElementById("telefono").innerHTML= "1122345567";
+}
+
+
+document.getElementsByClassName("btn btn-primary").onclick = function () {
+    document.getElementsByClassName("btn btn-primary").innerHTML= "vamos Boquita"
+}
